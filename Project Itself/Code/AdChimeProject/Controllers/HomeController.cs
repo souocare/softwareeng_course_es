e3f4d48230aeec4d11632b592ea91d3ed09273bd
souocare <38910349+souocare@users.noreply.github.com>
2022-11-09 19:11:35 +0000

    code start the project

---
+++ Project Itself/Code/AdChimeProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdChimeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        
+        
+    }
+}
---
+++ Project Itself/Code/AdChimeProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdChimeProject.MvcApplication" Language="C#" %>
